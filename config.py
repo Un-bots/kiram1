2dc6e51fcfd90344ef0143b29bc862b72a2b42dc
Un-bots <161623523+Un-bots@users.noreply.github.com>
2024-09-16 15:55:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/un-bots/jerry",
+    "https://github.com/un-bots/kiram1",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
