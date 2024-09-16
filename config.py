c2d044cb84ed63c38012bf222f3cde8b1b4d6105
Un-bots <161623523+Un-bots@users.noreply.github.com>
2024-09-16 16:11:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/unb_info")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/kira_update")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/unb_support")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
