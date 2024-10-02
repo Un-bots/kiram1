a9815c52294fd6d238a162a8ee9a614380700ebd
Un-bots <161623523+Un-bots@users.noreply.github.com>
2024-10-03 01:06:57 +0530

    Update play.py

--- AviaxMusic/plugins/play/play.py
+++ AviaxMusic/plugins/play/play.py
@@ -55,7 +55,7 @@ async def play_commnd(
 ):
 
       # Increment the play command count
-    await increment_play_command_count()
+   # await increment_play_command_count()
     
     mystic = await message.reply_text(
         _["play_2"].format(channel) if channel else _["play_1"]
